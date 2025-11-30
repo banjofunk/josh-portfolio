0533bf820faf510a4545750c2549804f9334e3dd
Josh Garner <josh.garner.dev@gmail.com>
2025-11-30 10:14:07 -0700

    cleanup lint

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -6,15 +6,15 @@ import { Cloud } from './Cloud';
  * @param {boolean} secondary - If true, uses dark text for light backgrounds
  */
 export const PageHeader = ({ secondary }) => (
-    <div className="w-full flex items-center justify-between p-4 pt-0 z-30">
-        <div className="flex flex-col space-y-2 font-serif text-right">
-            <h1 className={`text-2xl lg:text-7xl font-medium ${secondary ? 'text-gray-800' : 'text-white'}`}>
-                josh garner
-            </h1>
-            <h2 className="text-lg lg:text-2xl pr-2 text-sky-600">
-                full-stack developerrrr
-            </h2>
-        </div>
-        <Cloud className={`w-20 lg:w-52 ${secondary ? 'text-gray-800 opacity-90' : 'text-sky-800'}`} />
+  <div className="w-full flex items-center justify-between p-4 pt-0 z-30">
+    <div className="flex flex-col space-y-2 font-serif text-right">
+      <h1 className={`text-2xl lg:text-7xl font-medium ${secondary ? 'text-gray-800' : 'text-white'}`}>
+        josh garner
+      </h1>
+      <h2 className="text-lg lg:text-2xl pr-2 text-sky-600">
+        full-stack developer
+      </h2>
     </div>
+    <Cloud className={`w-20 lg:w-52 ${secondary ? 'text-gray-800 opacity-90' : 'text-sky-800'}`} />
+  </div>
 );
